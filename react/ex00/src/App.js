935a4590970a921bee96c61dca68f34fd7780e86
Jay <god007899@hanmail.net>
2019-10-19 11:58:23 +0900

    crp

--- react/ex00/src/App.js
+++ react/ex00/src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+const customer = [{
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'홍길동',
+  'birthday':'900525',
+  'gender':'남자',
+  'job':'백수'
+},
+{
+  'id':2,
+  'image':'https://placeimg.com/64/64/2',
+  'name':'이경호',
+  'birthday':'950525',
+  'gender':'남자',
+  'job':'교보핫트랙스'
+},
+{
+  'id':3,
+  'image':'https://placeimg.com/64/64/3',
+  'name':'김현창',
+  'birthday':'910525',
+  'gender':'남자',
+  'job':'진학사'
+}
+]
 
 class App extends Component {
   render() {
   return (
-    <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
+    <div>
+      {customer.map(c=>{
+        return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} 
+        />
+      })}
     </div>
     );
   }
